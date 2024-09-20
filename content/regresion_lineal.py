ff673c678a3288abecc3d1e7b3d0a2f1a89db2e0
Joaquin Malchansky Saleme <joacomk8@gmail.com>
2024-09-20 20:51:21 -0300

    nashe

--- content/regresion_lineal.py
+++ content/regresion_lineal.py
@@ -12,21 +12,25 @@ def regresion_lineal(X, y):
     syy = np.sum((y - y_prom)**2)
 
     # Pendiente (β1)
-#    beta_1 = sxy / sxx
+    beta_1 = sxy / sxx
     
     # Intersección con el eje y (β0)
-#    beta_0 = y_prom - beta_1 * X_prom
+    beta_0 = y_prom - beta_1 * X_prom
     
     # Predicciones
-#    y_pred = beta_0 + beta_1 * X
+    y_pred = beta_0 + beta_1 * X
     
     # Cálculo del coeficiente de correlación lineal
     R  = sxy / ((sxx * syy) ** 0.5)
 
     # Cálculo del coeficiente de determinación (R^2)
     # Este calculo no se usa porque terminé devolviendo R que es lo mismo, pero sirvió para corroborar que den lo mismo
-#    STC = np.sum((y - y_prom)**2)  # Suma total de cuadrados
-#    SCE = np.sum((y - y_pred)**2)  # Suma de los cuadrados de los errores
-#    R2 = 1 - (SCE / STC)
-    
+    STC = np.sum((y - y_prom)**2)  # Suma total de cuadrados
+    SCE = np.sum((y - y_pred)**2)  # Suma de los cuadrados de los errores
+    R2 = 1 - (SCE / STC)
+
+    #Calculos intervalos de confianza
+    print(f"El intervalo para b0 es [ {beta_0 - (-1.96009) * ((SCE/18942 * (1/len(X) + (X_prom**2) / sxx) ) ** 0.5)}, {beta_0 + (-1.96009) * ((SCE/18942 * (1/len(X) + (X_prom**2) / sxx) ) ** 0.5)} ]")
+    print(f"El intervalo para b1 es [ {beta_1 - (-1.96009) * ((((SCE/18942)**2) / sxx) ** 0.5)} , {beta_1 + (-1.96009) * ((((SCE/18942)**2) / sxx) ** 0.5)} ]")
+
     return R
